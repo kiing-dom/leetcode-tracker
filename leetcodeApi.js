8406644071a4ae3959c0d76a62e29b5862d30414
Dominion Gbadamosi <worshipgbadamosi@live.com>
2025-06-26 01:05:17 +0100

    feat: using leetcode graphql api to get problem data | also cleaned up code

--- leetcodeApi.js
+++ leetcodeApi.js
@@ -1,23 +1,4 @@
-async function fetchLeetCodeTags(slug) {
-  const query = {
-    query: `query getQuestionDetail($titleSlug: String!) { question(titleSlug: $titleSlug) { topicTags { name slug } } }`,
-    variables: { titleSlug: slug },
-  };
-
-  const response = await fetch("https://leetcode.com/graphql", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(query),
-    credentials: "same-origin",
-  });
-
-  if (!response.ok) return [];
-  const data = await response.json();
-  return data.data?.question?.topicTags?.map((tag) => tag.name) || [];
-}
-
+// Fetches title, difficulty, and tags for a LeetCode problem using the GraphQL API
 async function fetchLeetCodeProblemData(slug) {
   const query = {
     query: `query getQuestionDetail($titleSlug: String!) { question(titleSlug: $titleSlug) { title difficulty topicTags { name slug } } }`,
